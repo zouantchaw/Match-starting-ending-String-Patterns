0174d0ce8b63ca266d5ad150fa19e7dd114a8a19
Wielfried Zouantcha <44246692+zouantchaw@users.noreply.github.com>
2021-10-25 20:00:50 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// Match starting 
+// let firstString = "Ricky is first and can be found.";
+// let firstRegex = /^Ricky/;
+// firstRegex.test(firstString);
+// let notFirst = "You can't find Ricky now.";
+// firstRegex.test(notFirst);
+
+// Match ending
+let theEnding = "This is a never ending story";
+let storyRegex = /story$/;
+storyRegex.test(theEnding);
+let noEnding = "Sometimes a story will have to end";
+storyRegex.test(noEnding);
